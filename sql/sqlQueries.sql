dd2f167139c485a0cc31167c40af459b0e081763
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-01 19:54:59 -0400

    Query Fixes

--- sql/sqlQueries.sql
+++ sql/sqlQueries.sql
@@ -1,32 +1,36 @@
-1. What is the current stock of the blood inventory? 
-SELECT Quantity 
-FROM 'Blood Inventory';
+  -- 1. What is the current stock of the blood inventory? 
+SELECT `Blood type`, Quantity 
+FROM `Blood Inventory`;
 
 
-2. What is the hemoglobin count of a certain BloodBagNo? 
-SELECT 'hemoglobin count' 
-WHERE BloodBagNo = 13;
+  -- 2. What is the hemoglobin count of a certain BloodBagNo (ex. 25)? 
+SELECT `Hemoglobin Count`
+FROM `Blood`
+WHERE BloodBagNo = 25;
 
 
-3. What is the gender of the recipient of a specific blood type? 
-SELECT Sex FROM `Recipient` 
+  -- 3. What is the gender of the recipient of a specific blood type (ex. B+)? 
+SELECT Sex 
+FROM `Recipient` 
 WHERE `Blood Type`= 'B+';
 
 
-4. Which blood donors have a specific blood group?
+  -- 4. Which blood donors have a specific blood group (ex. AB-)?
 SELECT `First Name`, `Last Name`
-FROM `Blood Donor` WHERE `Blood Type` = 'AB-';
+FROM `Blood Donor`
+WHERE `Blood Type` = 'AB-';
 
 
-5. What is the contact information of a specific blood donor? 
-SELECT Phone FROM `Blood Donor` 
-WHERE `First Name` = 'Tom'; 
+  -- 5. What is the contact information of a specific blood donor (ex. Tom Brady)? 
+SELECT Phone 
+FROM `Blood Donor` 
+WHERE (`First Name` = 'Tom') AND (`Last Name` = 'Brady'); 
 
 
-6. What is the name of a specific blood type recipient? 
+6. What is/are the name(s) of a specific blood type recipient(s) (ex. AB)? 
 SELECT `First Name`, `Last Name` 
 FROM `Recipient`
-WHERE `Blood Type` = 'B+';
+WHERE `Blood Type` = 'B+;
 
 
 7. How many recipients are waiting for blood donations? 
@@ -45,37 +49,41 @@ WHERE `Blood Type` = 'B+';
 --Query here--
 
 
-11. What date did a specific patient receive the blood? 
+  -- 11. What date did a specific patient receive the blood (ex. Padme Amidala)? 
 SELECT `Date of Receive` 
 FROM `Recipient`
-WHERE (`First Name` like 'Padme' AND `Last Name` like 'Amidale');
+WHERE (`First Name` like 'Padme') AND (`Last Name` like 'Amidala');
 
 
 12. Which blood groups are going to expire soon? 
 --Query here--
 
 
-13. What is the cost of a specific blood transfusion? 
-SELECT Cost FROM Blood 
+  -- 13. What is the cost of a specific blood transfusion (ex. A-)? 
+SELECT Cost
+FROM `Blood`
 WHERE `Blood Type` = 'A-';
 
 
-14. What is the list of all available blood donors? 
-SELECT * FROM `Blood Donor`;
+  -- 14. What is the list of all available blood donors? 
+SELECT * 
+FROM `Blood Donor`;
 
 
 15. What is the name of the employee that worked with a specific patient on a specific date? 
 --Query here--
 
 
-16. What is the address of a certain employee? 
-SELECT Address FROM Employee 
-WHERE (`First Name` like 'Katniss' AND `Last Name` like 'Evedeen');
+  -- 16. What is the address of a certain employee (ex. Katniss Everdeen)? 
+SELECT Address 
+FROM Employee 
+WHERE (`First Name` like 'Katniss') AND (`Last Name` like 'Everdeen');
 
 
-17. What is the salary of a certain employee? 
-SELECT Salary FROM Employee 
-WHERE (`First Name` like 'Katniss' AND `Last Name` like 'Evedeen');
+  -- 17. What is the salary of a certain employee (ex. Katniss Everdeen)? 
+SELECT Salary 
+FROM Employee 
+WHERE (`First Name` like 'Katniss') AND (`Last Name` like 'Evedeen');
 
 
 18. How many blood donations took place today? 
@@ -86,8 +94,8 @@ WHERE (`First Name` like 'Katniss' AND `Last Name` like 'Evedeen');
 --Query here--
 
 
-20. How many visits did a specific blood recipient have so far?
-SELECT Vists FROM 
-`Recipient`
-WHERE (`First Name` like 'Ahsoka' AND `Last Name` like 'Tano');
+  -- 20. How many visits did a specific blood recipient have so far (ex. Ahsoka Tano)?
+SELECT Visits 
+FROM `Recipient`
+WHERE (`First Name` like 'Ahsoka') AND (`Last Name` like 'Tano');
 
