b63f9781c8f4cf9aa31e99118c0eee36be828979
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-03 17:41:18 -0400

    Update procedures.sql

--- Phase II/Code/procedures.sql
+++ Phase II/Code/procedures.sql
@@ -2,14 +2,14 @@
   
  -- Create Procedures for Database
 
---For query 3 
+
 CREATE PROCEDURE RecipientGenderFromBloodType @Blood_Type VARCHAR2(5)
 AS
 SELECT Sex 
 FROM Recipient 
 WHERE Blood_Type= @Blood_Type;
 
---For query 4
+
 CREATE PROCEDURE DonorFromBloodType @Blood_Type VARCHAR2(5)
 AS
 SELECT First_Name, Last_Name
