e507585820b4612c0b01967c062b10368e49a720
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-03 16:45:31 -0400

    Update procedures.sql

--- Phase II/Code/procedures.sql
+++ Phase II/Code/procedures.sql
@@ -1,12 +1,17 @@
  -- Ihtesham Akbar, Megan Spiers
   
  -- Create Procedures for Database
- 
- -- Should Return Statements Are Accurate for Our Sample Data
- 
-CREATE PROCEDURE CapitalizeEmployeeName @First_Name VARCHAR2 (30), @Last_Name VARCHAR2 (30)
+
+--For query 3 
+CREATE PROCEDURE RecipientGenderFromBloodType @Blood_Type VARCHAR2(5)
 AS
-UPDATE Employee
-SET First_Name=UPPER(LEFT(@First_Name,1))+LOWER(SUBSTRING(@First_Name,2,LEN(@First_Name)))
-SET Last_Name=UPPER(LEFT(@Last_Name,1))+LOWER(SUBSTRING(@Last_Name,2,LEN(@Last_Name)))
-GO;
+SELECT Sex 
+FROM Recipient 
+WHERE Blood_Type= @Blood_Type;
+
+--For query 4
+CREATE PROCEDURE DonorFromBloodType @Blood_Type VARCHAR2(5)
+AS
+SELECT First_Name, Last_Name
+FROM Blood_Donor
+WHERE Blood_Type = @Blood_Type;
