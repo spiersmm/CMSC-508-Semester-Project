188ba22d9f3658bd9c546c5a906d407df9984d52
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-03 14:13:14 -0400

    Create Permissions.sql

---
+++ Phase II/Code/Permissions.sql
@@ -0,0 +1,14 @@
+  -- Ihtesham Akbar, Megan Spiers
+
+ -- Grant Permissions
+ -- This does not work in gcloud as roles and permissions are set differently there; however, 
+ -- it was requested that this code be in our project.
+ 
+CREATE ROLE students;
+CREATE ROLE professor;
+
+GRANT students TO spiersmm, akbari;
+GRANT professor TO s2dmduke;
+
+GRANT SELECT ON Blood_Donor, Recipient, Employee, Blood_Inventory, Blood TO professor;
+GRANT ALL PRIVILEGES ON Blood_Donor, Recipient, Employee, Blood_Inventory, Blood TO students.
