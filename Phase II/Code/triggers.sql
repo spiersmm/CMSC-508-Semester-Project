354bb9b604b35adc0c70a3403a5db121d01773a8
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-03 17:53:59 -0400

    Update triggers.sql

--- Phase II/Code/triggers.sql
+++ Phase II/Code/triggers.sql
@@ -2,6 +2,14 @@
   
  -- Create Triggers for Database
  
- -- Should Return Statements Are Accurate for Our Sample Data
  
- 
+CREATE TRIGGER validateDate
+ BEFORE INSERT
+ ON
+ FOR EACH ROW
+BEGIN
+ IF NEW.Date_of_Donation NOT LIKE '%__/%__/%____%' THEN
+    SIGNAL SQLSTATE VALUE '45000'
+           SET Date_of_Donation = 'Date Not Valid';
+ END IF;
+END;
