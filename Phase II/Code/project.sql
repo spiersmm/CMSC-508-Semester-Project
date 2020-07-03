4c56e5f5af18d63450bddbcb5e175a94aa493969
Megan Spiers <36936205+CyberSpyers@users.noreply.github.com>
2020-07-03 14:12:12 -0400

    Update project.sql

--- Phase II/Code/project.sql
+++ Phase II/Code/project.sql
@@ -78,16 +78,3 @@ CREATE TABLE Blood(
  Cost VARCHAR(10)
 
 );
-
-
- -- Grant Permissions
- -- This does not work in gcloud as roles and permissions are set differently there; however, 
- -- it was requested that this code be in our project.
-CREATE ROLE students;
-CREATE ROLE professor;
-
-GRANT students TO spiersmm, akbari;
-GRANT professor TO s2dmduke;
-
-GRANT SELECT ON Blood_Donor, Recipient, Employee, Blood_Inventory, Blood TO professor;
-GRANT ALL PRIVILEGES ON Blood_Donor, Recipient, Employee, Blood_Inventory, Blood TO students.
