06776a78cf835b54baae5f69ecd6bd445c858297
shamakbar <53946273+shamakbar@users.noreply.github.com>
2020-07-04 14:33:33 -0400

    Update procedures.sql

--- Phase II/Code/procedures.sql
+++ Phase II/Code/procedures.sql
@@ -15,3 +15,28 @@ AS
 SELECT First_Name, Last_Name
 FROM Blood_Donor
 WHERE Blood_Type = @Blood_Type;
+
+
+
+delimiter /
+
+CREATE PROCEDURE HemoCount(IN c Float(10)) 
+
+BEGIN
+
+select Hemoglobin_Count
+from Blood
+Where BloodBagNo = c;
+
+End;
+
+/
+
+call HemoCount(0032) /
+
+
+
+
+
+
+
